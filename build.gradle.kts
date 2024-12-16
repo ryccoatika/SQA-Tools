6d2476f3539432e3dc029c1aebbf280e7985ef36
Rycco <ryccoatika@gmail.com>
2024-12-16 22:31:36 +0700

    ref: apply spotless plugin ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,28 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.kotlin.android) apply false
+  alias(libs.plugins.kotlin.compose) apply false
+  alias(libs.plugins.spotless)
+}
+
+allprojects {
+  // apply spotless plugin on all modules
+  apply(plugin = rootProject.libs.plugins.spotless.get().pluginId)
+  spotless {
+    kotlin {
+      target("**/*.kt")
+      targetExclude(
+        "${layout.buildDirectory.get().asFile}/**/*.kt",
+      )
+      ktlint(libs.versions.ktlint.get())
+    }
+    kotlinGradle {
+      target("**/*.kts")
+      targetExclude(
+        "${layout.buildDirectory.get().asFile}/**/*.kts",
+      )
+      ktlint(libs.versions.ktlint.get())
+    }
+  }
 }
