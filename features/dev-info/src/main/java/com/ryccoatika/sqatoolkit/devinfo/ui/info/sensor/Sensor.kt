bb5db14213161558115e15a184416440328fdd73
Rycco Atika <ryccoatika@gmail.com>
2026-08-03 17:32:59 +0700

    feat(dev-info): Sensor tab (expandable), fix Sensor order

--- features/dev-info/src/main/java/com/ryccoatika/sqatoolkit/devinfo/ui/info/sensor/Sensor.kt
+++ features/dev-info/src/main/java/com/ryccoatika/sqatoolkit/devinfo/ui/info/sensor/Sensor.kt
@@ -1,22 +1,55 @@
 package com.ryccoatika.sqatoolkit.devinfo.ui.info.sensor
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.ryccoatika.sqatoolkit.common.extensions.viewModel
 import com.ryccoatika.sqatoolkit.devinfo.R
+import com.ryccoatika.sqatoolkit.devinfo.ui.common.ItemComposer
 import com.ryccoatika.sqatoolkit.devinfo.ui.info.DevInfoType
+import com.ryccoatika.sqatoolkit.devinfo.ui.info.common.InfoViewState
 import me.tatarka.inject.annotations.Inject
 
 internal typealias Sensor = @Composable () -> Unit
 
 @Inject
 @Composable
-internal fun Sensor() {
-  Scaffold { padding ->
-    Text(text = "Sensor", modifier = Modifier.padding(padding))
-  }
+internal fun Sensor(
+  viewModelFactory: () -> SensorViewModel,
+) {
+  Sensor(
+    viewModel = viewModel(factory = viewModelFactory),
+  )
+}
+
+@Composable
+internal fun Sensor(
+  viewModel: SensorViewModel,
+) {
+  val viewState by viewModel.state.collectAsState()
+
+  Sensor(
+    state = viewState,
+  )
+}
+
+@Composable
+internal fun Sensor(
+  state: InfoViewState,
+) {
+  ItemComposer(
+    items = state.items,
+    modifier = Modifier
+      .padding(vertical = 8.dp, horizontal = 16.dp)
+      .verticalScroll(state = rememberScrollState())
+      .fillMaxSize(),
+  )
 }
 
 @Inject
@@ -25,7 +58,7 @@ internal class SensorType : DevInfoType {
     get() = DevInfoType.SENSOR_ID
 
   override val order: Int
-    get() = 3
+    get() = 7
 
   override val featureTitle: Int
     get() = R.string.di_text_sensor
